134efebed024f3f47ae614678794659dbfbf72b9
LongPT <p.thelong2000@gmail.com>
2024-01-16 22:50:36 +0700

    create websocket

--- src/main/java/com/kafka/kafkademo/ListenerService.java
+++ src/main/java/com/kafka/kafkademo/ListenerService.java
@@ -3,15 +3,18 @@ package com.kafka.kafkademo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ListenerService {
 
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
     @KafkaListener(topics = "output", groupId = "group_id")
     public void listen(@Payload String message) {
-
+        simpMessagingTemplate.convertAndSend("/topic/group", message);
         System.out.println("Received Message in group - group_id: " + message);
     }
 }
